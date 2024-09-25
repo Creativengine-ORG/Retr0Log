4f590f778a9c114e5f487211ac97a45299a7686d
Alexander Buchkov <retr0aalex@gmail.com>
2024-09-25 12:08:02 +0300

    Added The Log System

---
+++ src/Logger.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Retr0Log
+{
+    public class Logger
+    {
+        public string name;
+        public LogSettings settings;
+
+        public Logger(string name, LogSettings settings)
+        {
+            this.name = name;
+            this.settings = settings;
+        }
+
+        public void Log(string message, LogLevel level)
+        {
+            string time = DateTime.Now.ToString("h:mm:ss tt");
+
+            string finTime = settings.showTime ? "[" + time + "] " : "";
+            string finType = settings.showMessageLevel ? "[" + level.ToString().ToUpper() + "] " : "";
+            string finName = settings.showName ? name + ": " : "";
+
+            switch (level)
+            {
+                case LogLevel.Debug:
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    break;
+                case LogLevel.Info:
+                    Console.ForegroundColor = ConsoleColor.White;
+                    break;
+                case LogLevel.Warn:
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    break;
+                case LogLevel.Error:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    break;
+                case LogLevel.Fatal:
+                    Console.ForegroundColor = ConsoleColor.White;
+                    break;
+                case LogLevel.Success:
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    break;
+                default:
+                    break;
+            }
+
+            if (level == LogLevel.Fatal)
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+            }
+            else
+            {
+                Console.BackgroundColor = ConsoleColor.Black;
+            }
+
+            Console.WriteLine($"{finTime}{finType}{finName}{message}");
+        }
+    }
+
+    public struct LogSettings
+    {
+        public bool showTime;
+        public bool showName;
+        public bool showMessageLevel;
+
+        public LogSettings(bool showTime, bool showName, bool showMessageLevel)
+        {
+            this.showTime = showTime;
+            this.showName = showName;
+            this.showMessageLevel = showMessageLevel;
+        }
+    }
+
+    public enum LogLevel
+    {
+        Debug,
+        Info,
+        Warn,
+        Error,
+        Fatal,
+        Success
+    }
+}
--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Retr0Log;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +11,16 @@ namespace Example
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Create Logger
+            Logger myLog = new Logger("My Log", new LogSettings(true, true, true));
+
+            // Print Messages
+            myLog.Log("Hello World! This is Debug Message!", LogLevel.Debug);
+            myLog.Log("Hello World! This is Info Message!", LogLevel.Info);
+            myLog.Log("Hello World! This is Warning Message!", LogLevel.Warn);
+            myLog.Log("Hello World! This is Error Message!", LogLevel.Error);
+            myLog.Log("Hello World! This is Fatal Message!", LogLevel.Fatal);
+            myLog.Log("Hello World! This is Success Message!", LogLevel.Success);
 
             // Wait to see the result
             Console.ReadKey();
