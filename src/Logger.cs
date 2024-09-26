29ee09ef57cd5c7a84e8e4fd416b37fb06da171f
Alexander Buchkov <retr0aalex@gmail.com>
2024-09-26 10:06:45 +0300

    Added Summaries

--- src/Logger.cs
+++ src/Logger.cs
@@ -11,12 +11,33 @@ namespace Retr0Log
         public string name;
         public LogSettings settings;
 
+        /// <summary>
+        /// Initialize new Logger with custom name and settigs
+        /// </summary>
+        /// <param name="name">The name of the log</param>
+        /// <param name="settings">Custom settings for the logger</param>
         public Logger(string name, LogSettings settings)
         {
             this.name = name;
             this.settings = settings;
         }
 
+        /// <summary>
+        /// Initialize new Logger with custom name
+        /// </summary>
+        /// <param name="name">The name of the log</param>
+        public Logger(string name)
+        {
+            this.name = name;
+            
+            settings = new LogSettings(false, true, false);
+        }
+
+        /// <summary>
+        /// Prints message to the log
+        /// </summary>
+        /// <param name="message">The message to display</param>
+        /// <param name="level">The level of the log indicates if it is important or not</param>
         public void Log(string message, LogLevel level)
         {
             string time = DateTime.Now.ToString("h:mm:ss tt");
