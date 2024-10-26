9156495e3266017d869573f6dfb14f1a6d8ffc20
Alexander Buchkov <135352473+Retr0Aa@users.noreply.github.com>
2024-10-26 14:11:15 +0300

    Changed Premake Structure

--- premake5.lua
+++ premake5.lua
@@ -1,34 +1,15 @@
 -- premake5.lua
-workspace "Retr0Log"
-   configurations { "Debug", "Release" }
+project "Retr0Log"
+   kind "SharedLib"
+   language "C#"
+   targetdir "bin/%{cfg.buildcfg}"
 
-    project "Retr0Log"
-       kind "SharedLib"
-       language "C#"
-       targetdir "bin/%{cfg.buildcfg}"
+   files { "**.cs" }
 
-       files { "**.cs" }
+   filter "configurations:Debug"
+      defines { "DEBUG" }
+      symbols "On"
 
-       filter "configurations:Debug"
-          defines { "DEBUG" }
-          symbols "On"
-
-       filter "configurations:Release"
-          defines { "NDEBUG" }
-          optimize "On"
-
-    project "Example"
-        kind "ConsoleApp"
-        language "C#"
-        targetdir "bin/%{cfg.buildcfg}"
-        links { "Retr0Log" }
-
-       files { "**.cs" }
-
-       filter "configurations:Debug"
-          defines { "DEBUG" }
-          symbols "On"
-
-       filter "configurations:Release"
-          defines { "NDEBUG" }
-          optimize "On"
+   filter "configurations:Release"
+      defines { "NDEBUG" }
+      optimize "On"
